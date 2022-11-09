83af2d066312d918aea646b511f71932ab942727
Philippe20033 <philippesantos2003@gmail.com>
2022-11-09 10:02:47 +0100

    merge with DEV and end of branch

--- modules/mod_connexion/cont_connexion.php
+++ modules/mod_connexion/cont_connexion.php
@@ -1,58 +1,61 @@
 <?php
 require_once __DIR__ . "/modele_connexion.php";
 require_once __DIR__ . "/vue_connexion.php";
-require_once __DIR__ . "/vue_creation_compte.php";
 
-class ContConnexion {
+class ContConnexion
+{
     public function __construct()
     {
         $this->m = new ModeleConnexion();
         $this->v = new VueConnexion();
-        $this->vI = new VueInscription();
         $this->action = isset($_GET['action']) ? $_GET['action'] : "form_connexion";
         $this->exec();
     }
 
-    public function form_connexion() {
+    public function form_connexion()
+    {
         $this->v->formConnexion();
     }
 
-    public function form_inscription() {
-        $this->vI->formInscription();
+    public function form_inscription()
+    {
+        $this->v->formInscription();
     }
 
-    public function inscrit() {
+    public function inscrit()
+    {
         if ($this->m->form_ajout()) {
             $this->action = "form_connexion";
             $this->exec();
-        }
-        else {
+        } else {
             $this->action = "form_inscription";
             $this->exec();
         }
     }
 
-    public function connexion() {
+    public function connexion()
+    {
         if ($this->m->verif_connexion()) {
             $this->action = "connected";
             $this->exec();
-        }
-        else {
+        } else {
             $this->action = "form_connexion";
             $this->exec();
         }
     }
 
-    public function connected() {
+    public function connected()
+    {
         echo "connected";
     }
 
-    public function exec() {
-        switch($this->action) {
+    public function exec()
+    {
+        switch ($this->action) {
             case "form_connexion":
                 $this->form_connexion();
                 break;
-            case "form_register":
+            case "form_inscription":
                 $this->form_inscription();
                 break;
             case "connected":
@@ -64,7 +67,17 @@ class ContConnexion {
                 session_destroy();
                 unset($_SESSION['newsession']);
                 break;
+            case "new_inscription":
+                if ($this->m->form_ajout()) {
+                    echo "tout c'est bien passer";
+                    $this->action = "form_connexion";
+                    $this->exec();
+                } else {
+                    echo "tout c'est mal passer";
+                }
+                break;
         }
     }
 }
+
 ?>
--- modules/mod_connexion/vue_connexion.php
+++ modules/mod_connexion/vue_connexion.php
@@ -1,53 +1,131 @@
-
-
 <?php
-    require_once "./vue_generique.php";
-    class VueConnexion extends VueGenerique {
-        public function __construct()
-        {
-            parent::__construct();
-        }
+require_once "./vue_generique.php";
 
-        public function formConnexion() {
-            ?>
-            <div id="Div_Connection" class="text-center pt-5">
+class VueConnexion extends VueGenerique
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function formConnexion()
+    {
+        ?>
+        <div id="Div_Connection" class="text-center pt-5">
             <main class="form-signin w-100 mt-5 m-auto">
                 <form action="./index.php?module=mod_connexion&action=connexion" method="POST">
                     <?php
                     if (!isset($_SESSION['newsession'])) {
-                    ?>
-                    <img class="mt-5 mb-4" src="sources/Images/logo.jpg" alt="" width="100" height="100">
-                    <h1 class="h3 mb-3 fw-normal">Connexion</h1>
+                        ?>
+                        <img class="mt-5 mb-4" src="sources/Images/logo.jpg" alt="" width="100" height="100">
+                        <h1 class="h3 mb-3 fw-normal">Connexion</h1>
 
-            <div class="form-floating">
-                <input type="text" class="form-control" id="floatingInput" name = "login" placeholder="name@example.com" value="proftest">
-                <label for="floatingInput">Adresse e-mail / Identifiant</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" name = "password" placeholder="Password" minlength="8" maxlength="20" value="proftest">
-                <label for="floatingPassword">Mot de passe</label>
-            </div>
+                        <div class="form-floating">
+                            <input type="text" class="form-control" id="floatingInput" name="login"
+                                   placeholder="name@example.com" value="proftest">
+                            <label for="floatingInput">Adresse e-mail / Identifiant</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="password" class="form-control" id="floatingPassword" name="password"
+                                   placeholder="Password" minlength="8" maxlength="20" value="proftest">
+                            <label for="floatingPassword">Mot de passe</label>
+                        </div>
 
-            <a id="TextePasEncoreDeCompte" href="https://example.com">pas encore de compte ?</a>
+                        <a id="TextePasEncoreDeCompte" href="https://example.com">pas encore de compte ?</a>
 
-                    <div class="checkbox mt-2 mb-2">
-                        <label>
-                            <input id="test" type="checkbox" value="remember-me" checked> Se souvenir de moi
-                        </label>
-                    </div>
-                    <button id="bouton_send" class="w-100 btn btn-lg btn-warning" type="submit">Connexion</button>
-                    <p class="mt-5 mb-3 text-muted">&copy; 2022–2023</p>
+                        <div class="checkbox mt-2 mb-2">
+                            <label>
+                                <input id="test" type="checkbox" value="remember-me" checked> Se souvenir de moi
+                            </label>
+                        </div>
+                        <button id="bouton_send" class="w-100 btn btn-lg btn-warning" type="submit">Connexion</button>
+                        <p class="mt-5 mb-3 text-muted">&copy; 2022–2023</p>
 
-                    <?php
-                    }
-                    else {
-                        echo "t'es deja co mec";
+                        <?php
+                    } else {
+                        echo "Merci de bien vouloir vous déconectez avant de vous reconnecter";
                     }
                     ?>
                 </form>
             </main>
         </div>
         <?php
-        }
     }
+
+    public function formInscription()
+    {
+        ?>
+        <form action="./index.php?module=mod_connexion&action=new_inscription" method="POST">
+            <div class="container">
+                <main>
+                    <div class="pt-5 text-center">
+                        <img class="d-block mx-auto mb-0" src="sources/Images/logo.jpg" alt="" width="100"
+                             height="100">
+                    </div>
+
+                    <div class="row g-5">
+                        <div class="text-center pt-5">
+                            <h4 class="mb-1">Inscription</h4>
+                        </div>
+
+                        <form class="needs-validation" novalidate>
+
+                            <div id="formulaire-saisie-inscription" class="row g-3 col-md-5">
+
+                                <div class="col-sm-6">
+                                    <label for="motDePasse1" class="form-label">Mot de passe</label>
+                                    <input type="text" class="form-control" id="firstName" placeholder="1234" value=""
+                                           name="passwd" required minlength="8" maxlength="20">
+                                    <div class="invalid-feedback">Un mot de passe est requis</div>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <label for="motDePasse2" class="form-label">Confirmation du mot de passe</label>
+                                    <input type="text" class="form-control" id="lastName" placeholder="1234" value=""
+                                           required>
+                                    <div class="invalid-feedback">Une validation du mot de passe est requis</div>
+                                </div>
+
+                                <div class="col-12">
+                                    <label for="username" class="form-label">Nom d'utilisateur</label>
+                                    <div class="input-group has-validation">
+                                        <span class="input-group-text">#</span>
+                                        <input type="text" class="form-control" id="username" placeholder="Username"
+                                               name="username" required maxlength="20">
+                                        <div class="invalid-feedback">Un nom d'utilisateur est requis</div>
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <label for="email" class="form-label">Email <span class="text-muted"></span></label>
+                                    <input type="email" class="form-control" id="email"
+                                           placeholder="utilisateur@example.com" name="emailAdress">
+                                    <div class="invalid-feedback">Veuillez entrer une adresse e-mail valide</div>
+                                </div>
+
+                                <button id="bouton_send_incription" class="w-100 btn btn-primary btn-lg" type="submit">
+                                    Inscription
+                                </button>
+                        </form>
+                    </div>
+                </main>
+
+                <footer class="my-5 pt-0 text-muted text-center text-small">
+                    <p class="mb-1">&copy; 2022–2023 EAXO</p>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a
+                                    href="https://www.cnil.fr/fr/reglement-europeen-protection-donnees">Vie privé</a>
+                        </li>
+                        <li class="list-inline-item"><a href="https://github.com/DUT-Info-Montreuil/eaxo">Code</a></li>
+                        <li class="list-inline-item"><a
+                                    href="https://github.com/DUT-Info-Montreuil/eaxo/blob/main/README.md">Support</a>
+                        </li>
+                    </ul>
+                </footer>
+            </div>
+        </form>
+        <?php
+    }
+}
+
 ?>

4def20392cdf10aa7c01f281dc305d9e36ed9ae2
Philippe20033 <philippesantos2003@gmail.com>
2022-11-09 10:02:47 +0100

    merge with DEV and end of branch

--- modules/mod_connexion/modele_connexion.php
+++ modules/mod_connexion/modele_connexion.php
@@ -1,18 +1,21 @@
 <?php
 
 require_once "./connexion.php";
-class ModeleConnexion extends Connexion{
+
+class ModeleConnexion extends Connexion
+{
     public function __construct()
     {
-        
+
     }
 
     //Inserts the data (emailAdress, username and password) in the database, returns True if it worked, False otherwise
-    public function form_ajout() {
-        $sth = self::$bdd->prepare('INSERT INTO accounts values(NULL, :username, :password, :email)');
+    public function form_ajout()
+    {
+        $sth = self::$bdd->prepare('INSERT INTO users (email, username, passwd) values(:email, :username, :password)');
         if (!$this->verif_username($_POST['username'])) {
             if (!$this->verif_email($_POST['emailAdress'])) {
-                $sth->execute(array(':login'=>$_POST['username'], ':mdp'=>password_hash($_POST['passwd'], PASSWORD_DEFAULT), ':email'=>$_POST['email']));
+                $sth->execute(array(':username' => $_POST['username'], ':password' => password_hash($_POST['passwd'], PASSWORD_DEFAULT), ':email' => $_POST['emailAdress']));
                 return true;
             }
         }
@@ -20,11 +23,12 @@ class ModeleConnexion extends Connexion{
     }
 
     //Checks if the couple (username or email adress/password) exists in the database, returns True if it does, False otherwise
-    public function verif_connexion() {
+    public function verif_connexion()
+    {
         if (isset($_SESSION)) {
             $sql = 'SELECT * FROM users WHERE (username=:login) OR (email=:login)';
             $sth = self::$bdd->prepare($sql);
-            $sth->execute(array(':login'=>$_POST['login']));
+            $sth->execute(array(':login' => $_POST['login']));
             $result = $sth->fetch();
             if ($result) {
                 if (password_verify($_POST['password'], $result['passwd'])) {
@@ -37,21 +41,24 @@ class ModeleConnexion extends Connexion{
     }
 
     //Checks if the username exists in the database, returns True if it does, False otherwise
-    public function verif_username($username) {
+    public function verif_username($username)
+    {
         $sql = 'SELECT * FROM users WHERE (username=:login)';
         $sth = self::$bdd->prepare($sql);
-        $sth->execute(array(':login'=>$username));
+        $sth->execute(array(':login' => $username));
         $tab = $sth->fetch();
         return $tab;
     }
 
     //Checks if the email adress exists in the database, returns True if it does, False otherwise
-    public function verif_email($email) {
+    public function verif_email($email)
+    {
         $sql = 'SELECT * FROM users WHERE (email=:email)';
         $sth = self::$bdd->prepare($sql);
-        $sth->execute(array(':email'=>$email));
+        $sth->execute(array(':email' => $email));
         $tab = $sth->fetch();
         return $tab;
     }
 }
+
 ?>
--- template.php
+++ template.php
@@ -1,37 +1,48 @@
 <!DOCTYPE html>
-<html>
-    <head>
+<html lang="fr">
+<head>
     <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="./style.css">
-        <title>Home</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-        
-        <link rel="icon" type="image/png" href="sources/Images/logo.jpg" />
-        <base href="/eaxo/"> 
-    </head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+
+    <!-- ajout de bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
 
 
-    <body>
-        <?php
-            
-            require_once "./composants/composant_navbar/composant_navbar.php";
-            $nb = new ComposantNavBar();
-            
-            $nb->affichage();
-            echo $c->result;
-            
-            
-        ?>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
+    <link rel="icon" type="image/png" href="sources/Images/logo.jpg"/>
+    <base href="/eaxo/">
+    <link rel="stylesheet" href="./style.css">
+</head>
 
 
-        <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <!--!-->
-    <script src="./resources/scripts/widgets.js" type="module"></script>
-    <script src="./resources/scripts/fonts_controller.js"  type="module"></script>
-    </body>
+<body>
+<?php
+
+require_once "./composants/composant_navbar/composant_navbar.php";
+
+$nb = new ComposantNavBar();
+
+$nb->affichage();
+if (isset($c))
+    echo $c->result;
+else
+    echo "Erreur lor de la lecture d'une variable ligne 32 fichier template.php"
+
+
+?>
+
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
+<!--!-->
+<script src="./resources/scripts/widgets.js" type="module"></script>
+<script src="./resources/scripts/fonts_controller.js" type="module"></script>
+</body>
 </html>

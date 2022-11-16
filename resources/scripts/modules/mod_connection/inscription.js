7babe4034eb90c6ca486b9352bd3bc62a0b62d82
Philippe20033 <philippesantos2003@gmail.com>
2022-11-17 00:00:57 +0100

    improved password verification

--- resources/scripts/modules/mod_connection/inscription.js
+++ resources/scripts/modules/mod_connection/inscription.js
@@ -2,9 +2,29 @@
 $('DOMContentLoaded', function() {
 
     var colorBoutonInscription = $("#bouton_send_incription").css("background-color");
+    var marginInscriptionDiv = $("#formulaire-saisie-inscription").css("margin-left");
 
-    $("#motDePasse2").focusout(function (){verrifierMotDePasseEgal();});
-    $("#motDePasse1").focusout(function (){verrifierMotDePasseEgalSaisie1();});
+    $("#motDePasse1").focusout(function (){
+        verrifierMotDePasseEgalSaisie1();});
+    $("#motDePasse1").change(function (){
+        verifierForceMotPasse();
+    });
+    $("#motDePasse1").keyup(function (){
+        verifierForceMotPasse();
+    });
+    $("#motDePasse1").focusin(function (){
+        $("#forceMotPasseMinimal").css("display", "block");
+        $("#formulaire-saisie-inscription").css("margin-left", "50px");
+    });
+    $("#motDePasse1").focusout(function (){
+        if(verifierForceMotPasse()) {
+            $("#forceMotPasseMinimal").css("display", "none");
+            $("#formulaire-saisie-inscription").css("margin-left", marginInscriptionDiv);
+        }
+    });
+
+    $("#motDePasse2").focusout(function (){
+        verrifierMotDePasseEgal();});
 
     function verrifierMotDePasseEgalSaisie1() {
         if($("#motDePasse2").val() != ""){
@@ -28,14 +48,60 @@ $('DOMContentLoaded', function() {
 
     function activerBoutonInscription(){
         $("#bouton_send_incription").enabled;
-        $("#bouton_send_incription").css("background-color", colorBoutonInscription);
+        //$("#bouton_send_incription").css("background-color", colorBoutonInscription);
         $("#bouton_send_incription").css("opacity", "1");
     }
 
     function desactiverBoutonInscription() {
         $("#bouton_send_incription").disabled;
-        $("#bouton_send_incription").css("background-color", "grey");
+        //$("#bouton_send_incription").css("background-color", "grey");
         $("#bouton_send_incription").css("opacity", "0.5");
     }
+
+    function verifierForceMotPasse(){
+        var parfait = true;
+        //Taille
+        if($("#motDePasse1").val().length >= 8){
+            $("#characteresMDP").css("color", "green");
+        }else{
+            $("#characteresMDP").css("color", "red");
+            parfait = false;
+        }
+
+        //Majuscule
+        if(/^(?=.*[A-Z])/.test($("#motDePasse1").val())){
+            $("#MajusculeMDP").css("color", "green");
+        }else{
+            $("#MajusculeMDP").css("color", "red");
+            parfait = false;
+        }
+
+        //Minuscule
+        if(/^(?=.*[a-z])/.test($("#motDePasse1").val())){
+            $("#MinusculeMDP").css("color", "green");
+        }else{
+            $("#MinusculeMDP").css("color", "red");
+            parfait = false;
+        }
+
+        //Chiffre
+        if(/^(?=.*\d)/.test($("#motDePasse1").val())){
+            $("#chifreMDP").css("color", "green");
+        }else{
+            $("#chifreMDP").css("color", "red");
+            parfait = false;
+        }
+
+        //Charactere Special
+        if(/^(?=.*[\$@%*+\-_!])/.test($("#motDePasse1").val())){
+            $("#CharactereMDP").css("color", "green");
+        }else{
+            $("#CharactereMDP").css("color", "red");
+            parfait = false;
+        }
+
+        return parfait;
+    }
+
 
 });

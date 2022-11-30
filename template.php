e82e4ad2e59375c97c6f5280af4021720c038204
Philippe20033 <philippesantos2003@gmail.com>
2022-11-30 12:20:23 +0100

    Bux Merge Fix

--- template.php
+++ template.php
@@ -9,13 +9,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="./style.css">
 
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 
     <link rel="icon" type="image/png" href="resources/images/logo.jpg"/>
     <base href="<?php __DIR__ ?>">
+    <link rel="stylesheet" href="./style.css">
 
 </head>
 
